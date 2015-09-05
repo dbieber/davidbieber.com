88bc67634ff56b61bd5769f03615ccda956073a4
David Bieber <dbieber@princeton.edu>
2015-09-05 12:24:00 -0700

    Require SSL by default

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import argparse
 import os
 import sys
 
+import tornado.httpserver
 import tornado.ioloop
 import tornado.web
 import tornado.auth
@@ -12,40 +14,60 @@ from sendlater.sendlater import handlers as sendlater_handlers
 from thyme.thyme import handlers as thyme_handlers
 
 
-handlers = (
-    auth_handlers +
-    sendlater_handlers +
-    thyme_handlers +
-    [
-        (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': 'static', 'default_filename': 'index.html'}),
-        (r'/(.*)', tornado.web.StaticFileHandler, {'path': 'default', 'default_filename': 'index.html'}),
-    ]
-)
-
-tornado_settings = dict(
-    debug=settings.secure.debug,
-    xsrf_cookies=True,
-    cookie_secret=settings.secure.cookie_secret,
-    login_url='/auth/google',
-    google_oauth=dict(
-        key=settings.secure.google_oauth_key,
-        secret=settings.secure.google_oauth_secret,
-    )
-)
-
-application = tornado.web.Application(
-    handlers,
-    **tornado_settings
-)
-
 def main():
-    try:
-        port = int(sys.argv[1])
-    except:
-        # TODO(Bieber): Use local settings
-        port = 8000
+    parser = argparse.ArgumentParser(description='Launch davidbieber.com')
+    parser.add_argument('--ssl', default=True, action='store_true',
+                        help='Require ssl')
+    parser.add_argument('--no-ssl', dest='ssl', action='store_false',
+                        help='Do not require ssl')
+    parser.add_argument('--certfile', type=str, default=None, help='Path to SSL certfile')
+    parser.add_argument('--keyfile', type=str, default=None, help='Path to SSL keyfile')
+    parser.add_argument('--port', type=int, default=8000, help='Port for davidbieber.com')
 
-    application.listen(port)
+    args = parser.parse_args()
+    ssl = args.ssl
+    certfile = args.certfile
+    keyfile = args.keyfile
+    port = args.port
+
+    handlers = (
+        auth_handlers +
+        sendlater_handlers +
+        thyme_handlers +
+        [
+            (r'/static/(.*)', tornado.web.StaticFileHandler, {'path': 'static', 'default_filename': 'index.html'}),
+            (r'/(.*)', tornado.web.StaticFileHandler, {'path': 'default', 'default_filename': 'index.html'}),
+        ]
+    )
+
+    tornado_settings = dict(
+        debug=settings.secure.debug,
+        xsrf_cookies=True,
+        cookie_secret=settings.secure.cookie_secret,
+        login_url='/auth/google',
+        google_oauth=dict(
+            key=settings.secure.google_oauth_key,
+            secret=settings.secure.google_oauth_secret,
+        )
+    )
+
+    application = tornado.web.Application(
+        handlers,
+        **tornado_settings
+    )
+
+    if ssl:
+        assert certfile is not None
+        assert keyfile is not None
+        server = tornado.httpserver.HTTPServer(application, ssl_options={
+            "certfile": certfile,
+            "keyfile": keyfile,
+        })
+        assert port == 443
+        server.listen(port)
+    else:
+        application.listen(port)
+
     tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
