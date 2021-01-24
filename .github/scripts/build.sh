5d00bcd5b9fe7683db21aa758a73b1f301c1d148
David Bieber <david810@gmail.com>
2021-01-24 04:13:39 +0000

    Automatic commit by GitHub Actions

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -1,6 +1,7 @@
 # Transform jsx to js.
 cd davidbieber.com-main
 npx babel hugo/assets/js-src/ --out-dir hugo/assets/js/ --presets react-app/prod
+parcel build --out-dir hugo/assets/js/ hugo/assets/js-src/margin-notes.js
 cd hugo
 ls
 # Generate the static site.
