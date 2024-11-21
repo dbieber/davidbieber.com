e09a97bbb4871235c4478b8a196a22c5c56e037e
David Bieber <david810@gmail.com>
2024-11-21 04:38:32 +0000

    Automatic commit by GitHub Actions

--- hugo/public/snippets/2024-11-20-claude-artifacts/JellybeanJar.jsx
+++ hugo/public/snippets/2024-11-20-claude-artifacts/JellybeanJar.jsx
@@ -10,30 +10,32 @@ const JellybeanJar = () => {
   }));
 
   return (
-    <div className="relative w-48 h-64 mx-auto">
-      {/* Jar lid */}
-      <div className="absolute top-0 left-1/2 -translate-x-1/2 w-24 h-6 bg-gray-300 rounded-t-full z-20" />
-      
-      {/* Jar body */}
-      <div className="absolute top-4 left-1/2 -translate-x-1/2 w-32 h-56 bg-gray-100 rounded-3xl overflow-hidden">
-        {/* Glass highlight */}
-        <div className="absolute top-0 left-2 w-2 h-full bg-white opacity-30 rounded-full" />
+    <div className="tailwind-scope">
+      <div className="relative w-48 h-64 mx-auto">
+        {/* Jar lid */}
+        <div className="absolute top-0 left-1/2 -translate-x-1/2 w-24 h-6 bg-gray-300 rounded-t-full z-20" />
         
-        {/* Jellybeans */}
-        {jellybeans.map((bean) => (
-          <div
-            key={bean.id}
-            className={`absolute w-4 h-3 ${bean.color} rounded-full transform rotate-45`}
-            style={{
-              left: `${bean.left}%`,
-              top: `${bean.top}%`,
-            }}
-          />
-        ))}
+        {/* Jar body */}
+        <div className="absolute top-4 left-1/2 -translate-x-1/2 w-32 h-56 bg-gray-100 rounded-3xl overflow-hidden">
+          {/* Glass highlight */}
+          <div className="absolute top-0 left-2 w-2 h-full bg-white opacity-30 rounded-full" />
+          
+          {/* Jellybeans */}
+          {jellybeans.map((bean) => (
+            <div
+              key={bean.id}
+              className={`absolute w-4 h-3 ${bean.color} rounded-full transform rotate-45`}
+              style={{
+                left: `${bean.left}%`,
+                top: `${bean.top}%`,
+              }}
+            />
+          ))}
+        </div>
+        
+        {/* Jar base */}
+        <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-32 h-2 bg-gray-300 rounded-full" />
       </div>
-      
-      {/* Jar base */}
-      <div className="absolute bottom-0 left-1/2 -translate-x-1/2 w-32 h-2 bg-gray-300 rounded-full" />
     </div>
   );
 };
